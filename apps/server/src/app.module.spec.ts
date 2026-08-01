1f0a75f4cc30dd1faa7ed3a641a867f38aee4232
fanqw <fanqw.dev@gmail.com>
2026-08-01 22:36:23 +0800

    test: cover application module wiring

---
+++ apps/server/src/app.module.spec.ts
@@ -0,0 +1,33 @@
+import { MODULE_METADATA } from '@nestjs/common/constants';
+import { APP_GUARD } from '@nestjs/core';
+import { AppController } from './app.controller';
+import { AppModule } from './app.module';
+import { CommonModule } from './common/common.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { JwtAuthGuard } from './modules/auth/jwt-auth.guard';
+import { CategoryModule } from './modules/category/category.module';
+import { CommodityModule } from './modules/commodity/commodity.module';
+import { PurchasePlaceModule } from './modules/purchase-place/purchase-place.module';
+import { UnitModule } from './modules/unit/unit.module';
+
+describe('AppModule', () => {
+  it('registers all application modules and the health controller', () => {
+    expect(Reflect.getMetadata(MODULE_METADATA.IMPORTS, AppModule)).toEqual([
+      CommonModule,
+      AuthModule,
+      CategoryModule,
+      UnitModule,
+      CommodityModule,
+      PurchasePlaceModule,
+    ]);
+    expect(Reflect.getMetadata(MODULE_METADATA.CONTROLLERS, AppModule)).toEqual([
+      AppController,
+    ]);
+  });
+
+  it('registers JwtAuthGuard as the global application guard', () => {
+    expect(Reflect.getMetadata(MODULE_METADATA.PROVIDERS, AppModule)).toEqual([
+      { provide: APP_GUARD, useClass: JwtAuthGuard },
+    ]);
+  });
+});
